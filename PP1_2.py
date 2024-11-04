f3abfd50c8905752da70c52798ef672b8b0a98cf
PeterZheng-ICD2O1-O2 <Peter.zheng@student.tdsb.on.ca>
2024-11-04 23:42:40 +0000

    a

--- PP1_2.py
+++ PP1_2.py
@@ -4,23 +4,39 @@
 
 def q1():
   #Write Assignment code here
-
+  name = "Alex"
+  print (name)
 def q2():
   #Write Assignment code here
+  num1 = 3  
+  num2 = 5
+  num3 = 2
 
+  print (num1 + num2 + num3)
 def q3():
   #Write Assignment code here
-
+  num1 = 21
+  num2 = 4
+  print(num1 // num2)
 def q4():
   #Write Assignment code here
-
+  float1 = 6.3
+  float2 = 3.0
+  print(float1/float2)
 def q5():
   #Write Assignment code here
-
+  word = "Hello World"
+  print(word)
+  word = "Goodbye World"
+  print (word)
 def q6():
   #Write Assignment code here
-
+  t = "True"
+  print(t)
 def q7():
+  num1 = 7
+  num2 = 2
+  print(num1 % num2)
   #Write Assignment code here
 
   
